5ea64f083fb6ba20663553d69b3f723ab7b8a7a6
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2024-06-16 00:19:13 -0600

    added tests

--- src/test/java/io/github/saulmmbp/configService/MicroservicesConfigServerApplicationTests.java
+++ src/test/java/io/github/saulmmbp/configService/MicroservicesConfigServerApplicationTests.java
@@ -1,13 +1,32 @@
 package io.github.saulmmbp.configService;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Map;
 
-@SpringBootTest
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.*;
+
+@SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
 class MicroservicesConfigServerApplicationTests {
 
+    	@Autowired
+    	private TestRestTemplate testRestTemplate;
+    
 	@Test
-	void contextLoads() {
+	public void configurationAvailable() {
+	    @SuppressWarnings("rawtypes")
+	    ResponseEntity<Map> entity = testRestTemplate.getForEntity("/app/cloud", Map.class);
+	    Assertions.assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
+	@Test
+	public void envPostAvailable() {
+	    @SuppressWarnings("rawtypes")
+	    ResponseEntity<Map> entity = testRestTemplate.getForEntity("/admin/env", Map.class);
+	    Assertions.assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
 }
