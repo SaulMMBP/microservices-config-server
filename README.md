b670de3dfe288768fbd4185568b5a83d326ea16e
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2024-06-15 22:48:39 -0600

    1st commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Configuration Service
+Service to centralize and manage microservice's configurations
+
+It connect to a github repository with the configuration files for every microservices connected to this service.
+
---
+++ src/test/java/io/github/saulmmbp/configService/MicroservicesConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.saulmmbp.configService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
